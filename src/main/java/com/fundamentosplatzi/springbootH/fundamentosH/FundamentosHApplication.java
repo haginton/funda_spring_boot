c9816b680db206aa30b6100c986881b1a6a40c2b
Harrison Martinez <jonathan.martinez@corusconsulting.com>
2022-10-01 16:32:02 -0500

    First Commit

---
+++ src/main/java/com/fundamentosplatzi/springbootH/fundamentosH/FundamentosHApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springbootH.fundamentosH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosHApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/springbootH/fundamentosH/FundamentosHApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springbootH.fundamentosH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosHApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
