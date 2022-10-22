734fa331c62cd28036f3b5c06f2c065b3de03727
Harrison Martinez <jonathan.martinez@corusconsulting.com>
2022-10-21 22:56:20 -0500

    Uso de properties con ejemplo de generación de POJO

--- src/main/java/com/fundamentosplatzi/springbootH/fundamentosH/FundamentosHApplication.java
+++ src/main/java/com/fundamentosplatzi/springbootH/fundamentosH/FundamentosHApplication.java
@@ -4,6 +4,7 @@ import com.fundamentosplatzi.springbootH.fundamentosH.bean.MyBean;
 import com.fundamentosplatzi.springbootH.fundamentosH.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springbootH.fundamentosH.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springbootH.fundamentosH.component.ComponentDependency;
+import com.fundamentosplatzi.springbootH.fundamentosH.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -17,13 +18,15 @@ public class FundamentosHApplication implements CommandLineRunner {
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyBeanWithProperties myBeanWithProperties;
+	private UserPojo userPojo;
 
 	@Autowired
-	public FundamentosHApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties){
+	public FundamentosHApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 	}
 	//Fin inyección de dependencia con constructor
 
@@ -39,10 +42,11 @@ public class FundamentosHApplication implements CommandLineRunner {
 
 	//implementación de la dependencia inyectada (llamar la funcion de la dependencia)
 	@Override
-	public void run(String... args) {
+	public void run(String... args) { //codigo que se ejecutara al iniciar la aplicación
 		componentDependency.saludar();
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
 		System.out.println(myBeanWithProperties.function());
+		System.out.println(userPojo.getEmail() + " - " + userPojo.getPassword());
 	}
 }
--- src/main/java/com/fundamentosplatzi/springbootH/fundamentosH/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/springbootH/fundamentosH/configuration/GeneralConfiguration.java
@@ -2,11 +2,14 @@ package com.fundamentosplatzi.springbootH.fundamentosH.configuration;
 
 import com.fundamentosplatzi.springbootH.fundamentosH.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springbootH.fundamentosH.bean.MyBeanWithPropertiesImplement;
+import com.fundamentosplatzi.springbootH.fundamentosH.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
     private String name;
