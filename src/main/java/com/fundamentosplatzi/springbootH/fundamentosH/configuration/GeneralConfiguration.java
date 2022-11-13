b4b0c9965aa19a594dad6304bdab8a1654808a38
Harrison Martinez <jonathan.martinez@corusconsulting.com>
2022-11-12 22:38:12 -0500

    Uso de anotación value para apuntar a properties

--- src/main/java/com/fundamentosplatzi/springbootH/fundamentosH/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/springbootH/fundamentosH/configuration/GeneralConfiguration.java
@@ -8,10 +8,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:connection.properties")
 @EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
@@ -23,6 +25,18 @@ public class GeneralConfiguration {
     @Value("${value.random}")
     private String random;
 
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @Value("${driver}")
+    private String driver;
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
+
     @Bean
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplement(name, apellido);
@@ -31,10 +45,10 @@ public class GeneralConfiguration {
     @Bean
     public DataSource dataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver"); //Controlador (nombre clase controladora)
-        dataSourceBuilder.url("jdbc:h2:mem:testdb"); //url jdbc, despues del h2 puedo definir un path para guardar la bd
-        dataSourceBuilder.username("SA") ;
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(driver); //Controlador (nombre clase controladora)
+        dataSourceBuilder.url(jdbcUrl); //url jdbc, despues del h2 puedo definir un path para guardar la bd
+        dataSourceBuilder.username(username) ;
+        dataSourceBuilder.password(password);
         return dataSourceBuilder.build();
     }
 }
