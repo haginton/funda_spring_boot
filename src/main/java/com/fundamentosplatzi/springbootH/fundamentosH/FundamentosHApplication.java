557cef6bd1aec923434f4dec6cab003ce1c68a7c
Harrison Martinez <jonathan.martinez@corusconsulting.com>
2022-12-24 17:34:46 -0500

    Rollback con la anotación transactional

--- src/main/java/com/fundamentosplatzi/springbootH/fundamentosH/FundamentosHApplication.java
+++ src/main/java/com/fundamentosplatzi/springbootH/fundamentosH/FundamentosHApplication.java
@@ -70,13 +70,16 @@ public class FundamentosHApplication implements CommandLineRunner {
 
 	private void saveWithErrorTransactional(){
 		User test1 = new User("TestTransactional1", "TestTransactional1@domain.com", LocalDate.now());
-		User test2 = new User("TestTransactional2", "TestTransactional1@domain.com", LocalDate.now());
+		User test2 = new User("TestTransactional2", "TestTransactional2@domain.com", LocalDate.now());
 		User test3 = new User("TestTransactional3", "TestTransactional1@domain.com", LocalDate.now());
-		User test4 = new User("TestTransactional4", "TestTransactional1@domain.com", LocalDate.now());
+		User test4 = new User("TestTransactional4", "TestTransactional4@domain.com", LocalDate.now());
 
 		List<User> users = Arrays.asList(test1, test2, test3, test4);
-
-		userService.saveTransactional(users);
+		try {
+			userService.saveTransactional(users);
+		}catch(Exception e){
+			LOGGER.info("Este es una exception dentro del metodo transaccional " + e);
+		}
 
 		userService.getAllUsers().stream()
 				.forEach(user -> LOGGER.info("Este es el usuario dentro del metodo transaccional " + user));
